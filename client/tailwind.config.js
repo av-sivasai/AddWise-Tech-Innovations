5bee051499909e8cb3444b2a8c81769b7c2e909e
AMULURU VENKATA SIVA SAI <venkatasivasai2626@gmail.com>
2026-04-02 00:55:16 +0530

    Add custom brand colors to tailwind.config.js
    Added custom brand colors to the Tailwind CSS theme.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -6,8 +5,22 @@ export default {
     "./src/ui/**/*.{tsx,ts,js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: "rgb(254, 252, 232)",
+          100: "rgb(254, 249, 195)",
+          200: "rgb(254, 240, 138)",
+          300: "rgb(253, 224, 71)",
+          400: "rgb(250, 204, 21)",
+          500: "rgb(234, 179, 8)",
+          600: "rgb(202, 138, 4)",
+          700: "rgb(161, 98, 7)",
+          800: "rgb(133, 77, 14)",
+          900: "rgb(113, 63, 18)"
+        }
+      }
+    }
   },
-  plugins: [],
-  presets: [require("./src\\ui/tailwind.config.js")]
-}
+  plugins: []
+};
